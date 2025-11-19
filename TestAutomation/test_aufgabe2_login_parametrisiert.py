14330d25aa9381b698901fdff300f516cba871c9
MarcMaier-QA <marc_maier1990@t-online.de>
2025-11-19 15:40:06 +0100

    Add driver fixture

--- TestAutomation/test_aufgabe2_login_parametrisiert.py
+++ TestAutomation/test_aufgabe2_login_parametrisiert.py
@@ -1,3 +1,11 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+
+@pytest.fixture
+def driver():
+    chrome_driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    yield driver
+    driver.quit()
