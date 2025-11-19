6ab89cc2523e5f769ea9fa8357bda0ea56cef68f
MarcMaier-QA <marc_maier1990@t-online.de>
2025-11-19 15:47:19 +0100

    Add user list and password

--- TestAutomation/test_aufgabe2_login_parametrisiert.py
+++ TestAutomation/test_aufgabe2_login_parametrisiert.py
@@ -2,6 +2,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+VALID_PASSWORD = "secret_sauce"
+USERS = [
+    ("standard_user"),
+    ("locked_out_user"),
+    ("problem_user"),
+    ("performance_glitch_user"),
+    ("error_user"),
+    ("visual_user"),
+]
 
 @pytest.fixture
 def driver():
