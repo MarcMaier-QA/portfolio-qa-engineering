001bb11f2c70c54538330d0ef0b76b9169b48cd0
MarcMaier-QA <marc_maier1990@t-online.de>
2025-12-09 12:25:45 +0100

    designed login fixture

--- TestAutomation/tests/conftest.py
+++ TestAutomation/tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from TestAutomation.pages.login_page import LoginPage
+from TestAutomation.utils.constants import TEST_USER_EMAIL, TEST_USER_PASSWORD
+
 
 @pytest.fixture
 def driver():
@@ -12,3 +15,21 @@ def driver():
     driver.implicitly_wait(3)
     yield driver
     driver.quit()
+
+
+@pytest.fixture
+def login(driver):
+    """
+    Führt den Login-Prozess aus und stellt den eingeloggten WebDriver bereit.
+    Diese Fixture hängt von der 'driver' Fixture ab.
+    """
+    login_page = LoginPage(driver)
+
+    # Navigieren zur Login-Seite
+    login_page.navigate_to_login()
+
+    # Login durchführen
+    login_page.login(TEST_USER_EMAIL, TEST_USER_PASSWORD)
+
+    # Wichtig: Wir geben den bereits eingeloggten driver zurück
+    return driver
