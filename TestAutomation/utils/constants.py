dabe746b4e03ee3bd9d7f331cc6bde2556edc151
MarcMaier-QA <marc_maier1990@t-online.de>
2025-12-10 17:23:11 +0100

    add new variables

--- TestAutomation/utils/constants.py
+++ TestAutomation/utils/constants.py
@@ -21,6 +21,22 @@ PRODUCT_APPLES = "Pink Lady Apples"
 PRODUCT_ORANGES = "Oranges"
 PRODUCT_PEARS = "Loose Pears"
 
+# # Einheitspreise
+PRICE_CHERRY = 2.50
+PRICE_PINK_LADY_APPLE = 2.50
+
+# Gesamtkosten der Produkte
+TWO_CHERRIES_SUBTOTAL = 5.00
+SEVEN_CHERRIES_SUBTOTAL = 17.50
+TEN_CHERRIES_SUBTOTAL = 25.00
+
+# Versandkosten
+SHIPPING_COST_STANDARD = 5.00
+SHIPPING_COST_FREE = 0.00    # ab 20€ Wahrenwert
+
+# Versandkosten Schwellenwert
+FREE_SHIPPING_THRESHOLD = 20.00 # Gratis Versand ab (20.00€)
+
 # Produkt-URLs (für die Rating-Tests)
 PRODUCT_ORANGES_URL = f"{BASE_URL}/product/66b3a57b3fd5048eacb4798f"
 PRODUCT_PEARS_URL = f"{BASE_URL}/product/66b3a57b3fd5048eacb47990"
