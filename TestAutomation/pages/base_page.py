8f919a950251daed3ebe3fd74dc5c1289ad07fce
MarcMaier-QA <marc_maier1990@t-online.de>
2025-12-15 22:13:23 +0100

    BasePage erstellt und methoden eingefügt

---
+++ TestAutomation/pages/base_page.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from TestAutomation.utils.constants import DEFAULT_WAIT_TIME
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, DEFAULT_WAIT_TIME)
+
+    def open(self, url: str):
+        self.driver.get(url)
+
+    def click(self, locator):
+        self.wait.until(EC.element_to_be_clickable(locator)).click()
+
+    def type(self, locator, text: str, clear: bool = True):
+        element = self.wait.until(EC.visibility_of_element_located(locator))
+        if clear:
+            element.clear()
+        element.send_keys(text)
+
+    def is_visible(self, locator) -> bool:
+        return self.wait.until(EC.visibility_of_element_located(locator)).is_displayed()
+
+    def get_text(self, locator) -> str:
+        return self.wait.until(EC.visibility_of_element_located(locator)).text
